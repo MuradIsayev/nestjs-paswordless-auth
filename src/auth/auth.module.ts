820ece262c4b50abedc8ca859db613ff195e9df6
MurAd <muradisayevazer@gmail.com>
2023-07-02 22:01:00 +0400

    Modules imported

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,18 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from 'src/users/users.module';
 import { MagicLoginStrategy } from './magiclogin.strategy';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService, MagicLoginStrategy],
 })
