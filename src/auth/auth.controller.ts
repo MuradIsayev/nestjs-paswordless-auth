a271e09b3219b07e5315d55812795550adbf5a30
MurAd <muradisayevazer@gmail.com>
2023-07-02 20:54:20 +0400

    2 api initialized

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post('login')
+  async login() {
+    // TODO: send magic link
+  }
+
+  @Post('login/callback')
+  callback() {
+    // TODO: generate JWT access token
+  }
+
+
 }
